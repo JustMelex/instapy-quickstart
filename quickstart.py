eed16ee7e4174bb49637b334bb69f10ccfcf503d
JustMelex <79217695+JustMelex@users.noreply.github.com>
2021-02-17 18:16:05 +0100

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -3,8 +3,8 @@ from instapy import InstaPy
 from instapy import smart_run
 
 # login credentials
-insta_username = ''
-insta_password = ''
+insta_username = ''melex_.
+insta_password = ''Maxr2007
 
 comments = ['Nice shot! @{}',
         'I love your profile! @{}',
